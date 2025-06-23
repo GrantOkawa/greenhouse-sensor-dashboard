714fc99b3d13e8d7b1e953a1264ce084230504f7
GrantOkawa <gokawa04@gmail.com>
2025-06-23 11:42:02 -0400

    Added comments to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// Header component to display the title and number of records 
 const Header: React.FC<{ dataLength: number }> = ({ dataLength }) => (
   <header className="header bg-white-10 backdrop-blur-sm border border-white-20 shadow-lg rounded-xl">
     <div className="flex justify-between items-center">
@@ -11,13 +12,7 @@ const Header: React.FC<{ dataLength: number }> = ({ dataLength }) => (
         <span className="record-count text-green-300">
           {dataLength} records
         </span>
-        <div className="live-indicator">
-          <span className="relative flex h-3 w-3">
-            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-            <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
-          </span>
-        </div>
-      </div>
+      </div> {/* Display the number of records */}
     </div>
   </header>
 );
